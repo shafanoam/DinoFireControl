c498252b250db29d2d629d01214b677d1b656c2a
singhari <110957426+singhari@users.noreply.github.com>
2025-07-12 15:18:15 -0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 import pyqtgraph as pg
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 
 print("finished importing")
 
@@ -15,26 +15,62 @@ print("finished importing")
 # there is a timer
 
 class MainWindow(QtWidgets.QMainWindow):
-    
-    
     def __init__(self):
         super().__init__()
         bigBoi =  pg.LayoutWidget()
         graphWidgies = bigBoi.addLayout(row=0, col=0)
         interfaceWidgies = bigBoi.addLayout(row=0, col=1)
-        thrustGraph = graphWidgies.addWidget(pg.PlotWidget(background="black"), row=0, col=0)
-        tempGraph = graphWidgies.addWidget(pg.PlotWidget(), row=0, col=1)
-        pressure_tank_Graph = graphWidgies.addWidget(pg.PlotWidget(), row=1, col=0)
-        pressure_combustionChamber_Graph = graphWidgies.addWidget(pg.PlotWidget(), row=1, col=1)
+
+        #creating graphs
+        thrustGraph  = pg.PlotWidget(background="black")
+        tempGraph = pg.PlotWidget()
+        pressure_tank_Graph = pg.PlotWidget()
+        pressure_combustionChamber_Graph = pg.PlotWidget()
+
+        #adding graphs
+        graphWidgies.addWidget(thrustGraph, row=0, col=0)
+        graphWidgies.addWidget(tempGraph, row=0, col=1)
+        graphWidgies.addWidget(pressure_tank_Graph, row=1, col=0)
+        graphWidgies.addWidget(pressure_combustionChamber_Graph, row=1, col=1)
+
+        #creating the interface quarters
+        actionButtons = pg.LayoutWidget()
+        valveButtons = pg.LayoutWidget()
+        piOutput = pg.GraphicsLayoutWidget()
+        misc = pg.GraphicsLayoutWidget()
+
+        #creating buttons for ActionButtons
+        self.firingButton = QtWidgets.QPushButton('FIRE')
+        self.abortButton= QtWidgets.QPushButton('ABORT')
+
+        #linking buttons
+        self.firingButton.clicked.connect(self.fire)
+        self.abortButton.clicked.connect(self.abort)
+
+        #adding the buttons to the layouts
+        actionButtons.addWidget(self.firingButton, row=0, col=0)
+        actionButtons.addWidget(self.abortButton, row=1, col=0)
+
+        #adding the interface widgies
+        interfaceWidgies.addWidget(actionButtons, row=0, col=0)
+        interfaceWidgies.addWidget(valveButtons, row=0, col=1)
+        interfaceWidgies.addWidget(piOutput, row=1, col=0)
+        interfaceWidgies.addWidget(misc, row=1, col=1)
+
+
         self.central = bigBoi
 
         self.setWindowTitle("DinoSore")
 
-        interfaceWidgies.addWidget(pg.GraphicsLayoutWidget(), row=0, col=0)
         # adding buttons and stuff ig
         
         # start the plotting stuff here ig
 
+
+    def fire(self):
+        print("FIRING")
+    def abort(self):
+        print("ABORTING")
 
 app = QtWidgets.QApplication([])
 main = MainWindow()
